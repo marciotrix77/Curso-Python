63b2220894104c68a49e844ee43b7ef21ea5f3c5
Marcio Alencar <marciotrix77@gmail.com>
2025-03-15 03:05:06 -0300

    Testes Resolvido

--- Projeto01/recipes/tests/test_urls.py
+++ Projeto01/recipes/tests/test_urls.py
@@ -1,5 +1,22 @@
+import pytest
 from django.test import TestCase
+from django.urls import reverse
 
-class RecipeURLsTest(TestCase):  # Corrigi "Teste" para "Test"
+# Create your tests here.
+# @pytest.mark.fast 
+class RecipeURLsTeste(TestCase):
     def test_the_pytest_is_ok(self):
-        ...
+        assert True
+
+    def test_recipe_home_is_correct(self):
+        url = reverse("recipes:home")
+        self.assertEqual(url,'/')
+
+    def test_recipe_category_is_correct(self):
+        url = reverse("recipes:category", kwargs={'category_id': 1})
+        self.assertEqual(url,'/recipes/category/1/')
+
+        
+    def test_recipe_detail_is_correct(self):
+        url = reverse("recipes:recipe", kwargs={'id': 1})
+        self.assertEqual(url,'/recipes/1/')
--- Projeto01/tests.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from django.test import TestCase
-
-# Create your tests here.
-# @pytest.mark.fast 
-class RecipeURLsTeste(TestCase):
-    def test_the_pytest_is_ok(self):
-        assert True
